f18dbeccc8f56d53ae94937be045bcc7f8d0f557
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-02-13 08:58:12 +0530

    Added Hello World

---
+++ GitEcllipse2/src/Main.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * @author Gagandeep
+ * @time 8:57:36 am
+ * @date 13-Feb-2020
+ */
+public class Main {
+//	D:\Capgemini\GitEcllipse2 is te root directory
+	public static void main(String[] args) {
+		System.out.println("Hellow World");
+	}
+
+}
